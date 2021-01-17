2fb882b54f999fc9b2efb6768713dad7f6850531
Jordy de Koning <jordy@nefkon.com>
2021-01-17 18:23:50 +0100

    finished sell command

--- app/Console/Commands/BuyCrypto.php
+++ app/Console/Commands/BuyCrypto.php
@@ -80,6 +80,5 @@ class BuyCrypto extends Command
                 'text' => $msg
             ]));
         }
-
     }
 }
--- app/Console/Commands/SellCrypto.php
+++ app/Console/Commands/SellCrypto.php
@@ -2,10 +2,16 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Cryptocurrency;
+use App\Notifications\CryptoInfoNotification;
 use Illuminate\Console\Command;
 
 class SellCrypto extends Command
 {
+    protected $btcModel;
+    protected $ethModel;
+    protected $dotModel;
+
     /**
      * The name and signature of the console command.
      *
@@ -28,6 +34,10 @@ class SellCrypto extends Command
     public function __construct()
     {
         parent::__construct();
+
+        $this->btcModel = Cryptocurrency::where('currency', 'btc')->first();
+        $this->ethModel = Cryptocurrency::where('currency', 'eth')->first();
+        $this->dotModel = Cryptocurrency::where('currency', 'dot')->first();
     }
 
     /**
@@ -37,6 +47,37 @@ class SellCrypto extends Command
      */
     public function handle()
     {
-        return 0;
+        $sendNotification = false;
+        // Receive price
+        $btc = (new CryptoPriceInfo)->getPrice('BTCEUR');
+        $eth = (new CryptoPriceInfo)->getPrice('ETHEUR');
+        $dot = (new CryptoPriceInfo)->getPrice('DOTEUR');
+
+        $msg = "";
+        // Check if price matched
+        if ((int)$btc >= $this->btcModel->sell_value) {
+            $sendNotification = true;
+            $msg .= "Bitcoin has reached the value of {$btc}. Your sell value is {$this->btcModel->sell_value} Sell now!";
+        }
+
+        // Check if price matched
+        if ((int)$eth >= $this->ethModel->sell_value) {
+            $sendNotification = true;
+            $msg .= "Ethereum has reached the value of {$eth}. Your sell value is {$this->ethModel->sell_value} Sell now!";
+        }
+
+        // Check if price matched
+        if ((int)$dot >= $this->dotModel->sell_value) {
+            $sendNotification = true;
+            $msg .= "Pokadot has reached the value of {$dot}. Your sell value is {$this->dotModel->sell_value} Sell now!";
+        }
+
+        // Only send message when the price matched sell value
+        if ($sendNotification) {
+            // Notify User
+            \Notification::send('update', new CryptoInfoNotification([
+                'text' => $msg
+            ]));
+        }
     }
 }
